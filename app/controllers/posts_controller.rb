4d29f42cb5db2fde91af9e884c4876bc2252c0ac
Juan Diego <jdsoteldo00@gmail.com>
2020-01-30 18:56:38 -0500

    crud 4 posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,22 +5,37 @@ class PostsController < ApplicationController
   end
 
   def show
-
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(posts_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @post.update(posts_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def destroy
+    if @post.destroy
+      redirect_to posts_path
+    end
   end
 
   private
