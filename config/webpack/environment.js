56083bbd72cb8b9655d9df45a5b772eaa4e35c90
Juan Diego <jdsoteldo00@gmail.com>
2020-09-17 17:15:44 -0400

    webpack error

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-  $: 'jquery/src/jquery',
-  jQuery: 'jquery/src/jquery',
-  Popper: ['popper.js', 'default']
-}))
+environment.plugins.append('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
 
-exports = environment
+module.exports = environment
