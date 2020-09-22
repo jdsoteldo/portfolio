b27a0d846803e67dceceb5ff2fc68affb0bd9986
Juan Diego <jdsoteldo00@gmail.com>
2020-09-22 15:52:39 -0400

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
+  config.action_mailer.default_url_options = { :host => 'https://jd-website-portfolio.herokuapp.com/' }
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
